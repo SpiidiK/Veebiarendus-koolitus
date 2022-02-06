b1dbaeba4faf1ff8ff951f95a2f26550ca04469c
SpiidiK <97437884+SpiidiK@users.noreply.github.com>
2022-02-06 19:07:03 +0200

    loeng 08
    kodune ülesanne

--- loeng08/tere.js
+++ loeng08/tere.js
@@ -3,12 +3,12 @@ while (kasutajaNimi == ''){
     kasutajaNimi = prompt("Sisesta nimi")
 }
 
-let tervitus = "Tere " + "<strong>" + kasutajaNimi + "</strong>" + " , küll sul on  suurepärane pikk nimi"
+let tervitus = "Tere " + "<strong>" + kasutajaNimi + "</strong>" + " , küll sul on  suurepärane pikk nimi" + "<p><img src=\"pikk.png\"></p>"
 let tervitusElement = document.getElementById("tervitus")
 
 if (kasutajaNimi.length <= 5) {
   
-    tervitus = "Tere " + "<strong>" + kasutajaNimi + "</strong>" + " ,  küll sul on ilus lühike nimi"
+    tervitus = "Tere " + "<strong>" + kasutajaNimi + "</strong>" + " , küll sul on ilus lühike nimi" + "<p><img src=\"lyhike.png\"></p>"
 }
 
 
